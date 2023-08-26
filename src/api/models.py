7561cbc10c811fa9eb0676d73909d8f596abe7bb
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-08-26 23:02:11 +0000

    fixed enum config on data models

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=False, nullable=False)
     last_name = db.Column(db.String(100), unique=False, nullable=False)
-    document_type = db.Column(db.Enum, unique=False, nullable=False)
+    document_type_enum = db.Enum('DNI', 'NIE', 'Passport', name='document_type_enum')
+    document_type = db.Column(document_type_enum, nullable=False)
     document_number = db.Column(db.String(20), unique=True, nullable=False)
     address_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con tabla address (address.id)
     phone = db.Column(db.String(20), unique=True, nullable=False)
@@ -15,7 +16,7 @@ class Users(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<Users {self.name + " " + self.last_name}>'
+        return f'<User {self.name + " " + self.last_name}>'
 
     def serialize(self):
         return {
@@ -54,12 +55,12 @@ class Address(db.Model):
             "city": self.city
         }
     
-class FavoriteUsers(db.Model):
+class FavoriteUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=True, nullable=True) # Relacionado con Users (users.id)
+    user_id = db.Column(db.Integer, unique=True, nullable=True) # Relacionado con User (user.id)
 
     def __repr__(self):
-        return f'<FavoriteUsers {self.id}>'
+        return f'<FavoriteUser {self.id}>'
 
     def serialize(self):
         return {
@@ -82,10 +83,11 @@ class FavoriteListings(db.Model):
     
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reviewer_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con Users (users.id)
-    receiver_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con Users (users.id)
+    reviewer_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con User (user.id)
+    receiver_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con User (user.id)
     comment = db.Column(db.String(1000), unique=False, nullable=True)
-    punctuation = db.Column(db.Enum, unique=False, nullable=False)
+    punctuation_enum = db.Enum('1', '2', '3', '4', '5', name='punctuation_enum')
+    punctuation = db.Column(punctuation_enum, nullable=False)
 
     def __repr__(self):
         return f'<Reviews {self.id}>'
@@ -99,16 +101,17 @@ class Reviews(db.Model):
             "punctuation": self.punctuation
         }
     
-class Lintings(db.Model):
+class Listings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    seller_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con Users (users.id)
+    seller_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con User (user.id)
     book_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con Books (books.id)
     album_id = db.Column(db.Integer, unique=True, nullable=True) # Relacionado con Album (album.id)
     listing_title = db.Column(db.String(200), unique=False, nullable=False)
     favorite_counter = db.Column(db.Integer, unique=False, nullable=False)
     sale_price = db.Column(db.Float, unique=False, nullable=False)
     description = db.Column(db.String(1000), unique=False, nullable=True)
-    status = db.Column(db.Enum(), unique=False, nullable=False)
+    status_enum = db.Enum('Activo', 'Reservado', 'Vendido', 'Cancelado', name='status_enum')
+    status = db.Column(status_enum, nullable=False)
 
     def __repr__(self):
         return f'<Listings {self.listing_title}>'
@@ -194,12 +197,13 @@ class Categories(db.Model):
     
 class Transactions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    seller_id = db.Column(db.Integer, unique=False, nullable=False) # Relacionado con Users (users.id)
-    buyer_id = db.Column(db.Integer, unique=False, nullable=False) # Relacionado con Users (users.id)
+    seller_id = db.Column(db.Integer, unique=False, nullable=False) # Relacionado con User (user.id)
+    buyer_id = db.Column(db.Integer, unique=False, nullable=False) # Relacionado con User (user.id)
     listing_id = db.Column(db.Integer, unique=True, nullable=False) # Relacionado con Listings (listings.id)
     date = db.Column(db.DateTime, unique=False, nullable=False)
     total = db.Column(db.Float, unique=False, nullable=False)
-    status = db.Column(db.Enum(), unique=False, nullable=False)
+    transaction_status_enum = db.Enum('Completada', 'Cancelada', 'Suspendida', 'Procesando', 'Error', name='transaction_status_enum')
+    status = db.Column(transaction_status_enum, nullable=False)
 
     def __repr__(self):
         return f'<Transactions {self.id}>'
