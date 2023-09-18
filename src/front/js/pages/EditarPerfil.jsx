ed3e9587346730cb862e9725e9002596457ef326
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-09-18 09:04:10 +0000

    put added

--- src/front/js/pages/EditarPerfil.jsx
+++ src/front/js/pages/EditarPerfil.jsx
@@ -5,6 +5,7 @@ import photo from "/workspaces/sp44-final-project-g3-readeeks/src/front/img/2.pn
 export const EditarPerfil = () => {
 
     const [user, setUser] = useState('');
+    const [newName, setNewName] = useState('');
 
     const fetchGetUsers = async (id) => {
     
@@ -24,6 +25,31 @@ export const EditarPerfil = () => {
         }
       }
     
+    const handleNameChange = async () => {
+
+      const url = `${process.env.BACKEND_URL}/users/${user.results.id}`; //Puede que haya que cambiar el user.id por userId
+
+      const request = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "Nombre": newName
+          })
+      };
+
+      const response = await fetch(url, request);
+
+      if (response.ok) {
+        console.log("Nombre actualizado correctamente");
+        setUser(prevUser => ({ ...prevUser, results: { ...prevUser.results, name: newName } }));
+    } else {
+        console.error("Error al actualizar el nombre", response.status, response.statusText);
+    }
+
+    }
+
       const userId = 1; //cambiar este id por la variable del ID del usuario logueado
     
       useEffect(() => {
@@ -36,20 +62,31 @@ export const EditarPerfil = () => {
 
         <div className="EditarPerfil-wrapper">
 
+            {user.status !== undefined && user.status !== "" ? (
             
-            <div className="EditarPerfil-img-profile">
+            <div className="EditarPerfil-Profile">
             
             <div className="EditarPerfil-img">
                 <img src={photo} alt="" className="" />
+                {/* AÑADIR EL BOTON DE CAMBIAR DE IMAGEN */}
             </div>
 
+            <div className="EditarPerfil-Information">
+                <h5>{user.results.name}</h5>
+                <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+                <button onClick={handleNameChange}>Cambiar Nombre</button>
+            </div>
 
+            </div>
             
-           
-            </div>
-        
+            ) : (
+                  
+                <div className="spinner-border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+             
+            )}
 
         </div>
-
     )
 }
