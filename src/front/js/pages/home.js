ab068dbeb13d7e5881bdce9cfa646fb6d8536db6
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-09-20 18:40:34 +0000

    jejeje

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,51 +3,53 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const [books, setBooks] = useState([]);
-  
-	const fetchGetBooks = async () => {
-	  try {
-		const url = `https://ubiquitous-invention-pxpv4j6pvv5h74r9-3001.app.github.dev/api/listings`;
-		const response = await fetch(url);
-  
-		if (!response.ok) {
-		  throw new Error(`Error ${response.status}: ${response.statusText}`);
-		}
-  
-		const data = await response.json();
-		setBooks(data.results.slice(0, 12));  // Limit to 12 items
-	  } catch (error) {
-		console.error("Error fetching books:", error);
-	  }
-	};
-  
-	useEffect(() => {
-	  fetchGetBooks();
-	}, []);
-  
-	return (
-	  <div className="Home-wrapper">
-		<div className="MyBooksList-main">
-		  <h5>Encuentra el libro perfecto para tu nueva aventura</h5>
-  
-		  <div className="MyBooksList-Component">
-			{books.length > 0 ? (
-			  books.map((item, index) => (
-				<div key={index} className="MyBooksList-BookImg">
-				  <img src={item.imageSrc} alt={item.listing_title} className="" />
-				  <p>
-					{item.listing_title}: <b>{item.sale_price}€</b>
-				  </p>
-				</div>
-			  ))
-			) : (
-			  <div className="spinner-border" role="status">
-				<span className="visually-hidden">Loading...</span>
-			  </div>
-			)}
-		  </div>
-		</div>
-	  </div>
-	);
+  const [books, setBooks] = useState([]);
+
+  const fetchGetBooks = async () => {
+    try {
+      const url = `https://ubiquitous-invention-pxpv4j6pvv5h74r9-3001.app.github.dev/api/listings`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      // Reorganizar aleatoriamente los libros antes de seleccionar los primeros 12
+      const shuffledBooks = data.results.sort(() => Math.random() - 0.5);
+      setBooks(shuffledBooks.slice(0, 12));
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
   };
-  
+
+  useEffect(() => {
+    fetchGetBooks();
+  }, []);
+
+  return (
+    <div className="Home-wrapper">
+      <div className="HomeList-main">
+        <h5>Encuentra el libro perfecto para tu nueva aventura</h5>
+
+        <div className="HomeList-Component">
+          {books.length > 0 ? (
+            books.map((item, index) => (
+              <div key={index} className="HomeList-BookImg">
+                <img src={item.imageSrc} alt={item.listing_title} className="" />
+                <p>
+                  {item.listing_title}: <b>{item.sale_price}€</b>
+                </p>
+              </div>
+            ))
+          ) : (
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
