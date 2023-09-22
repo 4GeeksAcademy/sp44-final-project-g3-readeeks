63927a5939fe7ce9742d28e7ef482bc9859803c5
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-09-22 11:43:36 +0000

    reset migrations before deploy

---
+++ migrations/versions/d6ae2a8469d4_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d6ae2a8469d4
+Revises: 
+Create Date: 2023-09-22 11:41:14.351034
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd6ae2a8469d4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
